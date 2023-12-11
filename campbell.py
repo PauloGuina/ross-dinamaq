a55800fef095fdf0c28fa9d10f8315f019823c56
PauloGuina <pauloguina14@gmail.com>
2023-12-10 21:30:59 -0300

    second commit

--- campbell.py
+++ campbell.py
@@ -11,7 +11,7 @@ pio.renderers.default = "notebook"
 # Material section
 
 # from E and G_s
-steel = rs.Material(name="Steel", rho=7810, E=211e9, G_s=81.2e9)
+steel = rs.Material(name="Steel", rho=7810, E=210e9, Poisson=0.3)
 steel.save_material()
 
 
@@ -25,29 +25,26 @@ bearing = rs.BearingElement(
 
 utils.Plot(bearing)
 
-n = 6
+
 
 shaft_elem = [
     rs.ShaftElement(
-        L=0.25,
-        idl=0.0,
-        odl=0.05,
+        L=0.7,
+        idl=2.0,
+        odl=3.75,
         material=steel,
         shear_effects=True,
         rotary_inertia=True,
         gyroscopic=True,
     )
-    for _ in range(n)
+    for _ in range(6)
 ]
 
 disk0 = rs.DiskElement.from_geometry(
-    n=2, material=steel, width=0.07, i_d=0.05, o_d=0.28
-)
-disk1 = rs.DiskElement.from_geometry(
-    n=4, material=steel, width=0.07, i_d=0.05, o_d=0.28
+    n=2, material=steel, width=0.07, i_d=2.0, o_d=3.75
 )
 
-disks = [disk0, disk1]
+disks = [disk0]
 
 stfx = 1e6
 stfy = 0.8e6
@@ -58,23 +55,20 @@ bearing2 = rs.BearingElement(7, kxx=stfx, kyy=stfy, cxx=0)
 
 bearings = [bearing0, bearing1, bearing2]
 
-pm0 = rs.PointMass(n=7, m=30)
+pm0 = rs.PointMass(n=7, m=0.3)
 
 pointmass = [pm0]
 
-rotor1 = rs.Rotor(shaft_elem, disks, bearings, pointmass)
+rotor1 = rs.Rotor(shaft_elem, disks, bearings,pointmass)
 
 
+samples = 30
 
-
-
-
-samples = 10
-speed_range = np.linspace(300, 1200, samples)
+speed_range = np.linspace(300, 3000, samples)
 
 campbell = rotor1.run_campbell(speed_range)
 
-campbell.plot()
+plot_campbell = campbell.plot(frequency_units="RPM",harmonics=[0.5,1])
 
-test.plot_figure(campbell.plot())
+test.plot_figure(plot_campbell)
 
--- test.py
+++ test.py
@@ -3,37 +3,41 @@ import matplotlib.pyplot as plt
 def plot_figure(figure):
     data = figure['data']
 
-    # Criar uma nova figura
+    # Create a new figure
     plt.figure(figsize=(10, 6))
 
-    # Mapeamento de estilos de marcadores
+    # Mapping of marker styles
     marker_mapping = {
         'triangle-up': '^',
         'circle': 'o',
         'triangle-down': 'v',
-        'x': 'x'
+        'x': 'x',
     }
 
-    # Plotar os dados sem valores zero
+    # Plot the data without zero values
     for trace in data:
         if trace['type'] == 'scatter':
             x_values = [x for x in trace['x'] if x != 0]
             y_values = [y for y in trace['y'] if y != 0]
 
             if trace['name'] == '1x speed':
-                plt.plot(x_values, y_values, label=trace['name'], color='blue', linestyle='dashdot')
+                # Plot 1x speed data only once
+                plt.plot(x_values, y_values, label=trace['name'], color='red', linestyle='dashdot')
+            elif trace['name'] == '0.5x speed':
+                # Plot 0.5x speed data with red dashdot style
+                plt.plot(x_values, y_values, label=trace['name'], color='orange', linestyle='dashdot')
             else:
                 marker_style = marker_mapping.get(trace['marker']['symbol'], 'o')
-                color = 'blue' if trace['legendgroup'] == 'Forward' else 'green' if trace['legendgroup'] == 'Mixed' else 'red'
+                color = 'blue' if trace['legendgroup'] == 'Forward' else 'red' if trace['legendgroup'] == 'Mixed' else 'red' if trace['legendgroup'] == 'Crit. Speed' else 'green'
                 plt.scatter(x_values, y_values, label=trace['name'], color=color, marker=marker_style)
 
-    # Configurar o layout
+    # Configure the layout
     layout = figure['layout']
     plt.title(layout['title']['text'])
     plt.xlabel(layout['xaxis']['title']['text'])
     plt.ylabel(layout['yaxis']['title']['text'])
     plt.legend(loc='upper right')
 
-    # Mostrar o gráfico
+    # Show the graph
     plt.grid(True)
     plt.show()
